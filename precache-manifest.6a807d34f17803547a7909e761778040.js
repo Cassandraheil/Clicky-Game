83b9c7703f7a47950ffc9f4b7799603fd57af2d1
Cassandra Heil <Cassandraheil19@gmail.com>
2020-05-05 20:42:27 -0400

    Updates

---
+++ precache-manifest.6a807d34f17803547a7909e761778040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fab4d514b14ee5705b27e92aa0cb224",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "72d0a8281d30e6fefe12",
+    "url": "/Clicky-Game/static/css/main.e130c616.chunk.css"
+  },
+  {
+    "revision": "e7a7d618357b6488d8ec",
+    "url": "/Clicky-Game/static/js/2.ea2e2277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.ea2e2277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d0a8281d30e6fefe12",
+    "url": "/Clicky-Game/static/js/main.33285c66.chunk.js"
+  },
+  {
+    "revision": "0990367c11f73e13d4b3",
+    "url": "/Clicky-Game/static/js/runtime-main.9ca74fd0.js"
+  }
+]);
